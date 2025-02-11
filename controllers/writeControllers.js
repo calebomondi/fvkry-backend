ef3c286510581e5e5e20773b4bab00164f99f846
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-02-11 15:30:12 +0300

    refactor: update lockAsset response format and export supabase as default

--- controllers/writeControllers.js
+++ controllers/writeControllers.js
@@ -1,13 +1,12 @@
-//import supabase from '../database/db.js'
+//
 
 //lock
 export const lockAsset = async (req, res) => {
-    const {data} = req.body;
-
     try {
-        console.log(`data: ${JSON.stringify(data)}`);
-        res.status(200).json({message: JSON.stringify(data)});
+
+        res.status(200).json({message: `${JSON.stringify(data)} address: ${address}`});
     } catch (error) {
         res.status(500).json({ message: 'Could Not Lock ETH!', error: error.message });
     }
 }
+
--- database/db.js
+++ database/db.js
@@ -3,4 +3,6 @@ import { config } from 'dotenv';
 
 config();
 
-export const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_ANON_KEY);
+
+export default supabase;
